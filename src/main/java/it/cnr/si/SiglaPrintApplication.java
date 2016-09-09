5c49247c9753b2da73021de1365ae3608330dab3
Francesco Uliana <francesco.uliana@cnr.it>
2016-09-09 15:53:12 +0200

    start.spring.io

---
+++ src/main/java/it/cnr/si/SiglaPrintApplication.java
@@ -0,0 +1,12 @@
+package it.cnr.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiglaPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiglaPrintApplication.class, args);
+	}
+}
---
+++ src/test/java/it/cnr/si/SiglaPrintApplicationTests.java
@@ -0,0 +1,16 @@
+package it.cnr.si;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiglaPrintApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
