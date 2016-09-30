8862b2b0cf19c6ed9ec5d1b7017a87a4d79c82dc
marco.spasiano <marco.spasiano@cnr.it>
2016-09-30 14:06:29 +0200

    autocommit

--- src/main/java/it/cnr/si/config/DatabaseConfiguration.java
+++ src/main/java/it/cnr/si/config/DatabaseConfiguration.java
@@ -1,11 +1,14 @@
 package it.cnr.si.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by francesco on 12/09/16.
@@ -16,13 +19,18 @@ public class DatabaseConfiguration {
 
     private DataSource dataSource;
 
+    @Value("${spring.jpa.properties.hibernate.connection.autocommit}")
+    private Boolean autocommit;
+    
     public DatabaseConfiguration (DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.dataSource = dataSource; 
     }
 
     @Bean
     public Connection connection () throws SQLException {
-        return dataSource.getConnection();
+    	Connection conn = dataSource.getConnection();
+    	conn.setAutoCommit(autocommit);
+        return conn;
     }
 
 }
