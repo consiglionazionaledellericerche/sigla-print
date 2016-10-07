41300ecebc37ce897490861ddcb2249781eceb62
marco.spasiano <marco.spasiano@cnr.it>
2016-10-07 09:01:27 +0200

    BUG recupero excel

--- src/main/java/it/cnr/si/web/PrintResource.java
+++ src/main/java/it/cnr/si/web/PrintResource.java
@@ -26,11 +26,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -89,8 +90,17 @@ public class PrintResource {
 
     }
 
+    @DeleteMapping("/api/v1/get/print/{user}/{name:.+}")
+    public ResponseEntity<byte[]> deletepdf(@PathVariable String user, @PathVariable String name) {
+        LOGGER.info("delete report from user: {} and name: {}", user, name);
+
+        String path = Arrays.asList(printOutputDir, user, name).stream().collect(Collectors.joining(fileSeparator));
+        new File(path).delete();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
     @GetMapping("/api/v1/get/excel/{name:.+}")
-    public ResponseEntity<byte[]> getxls(@RequestAttribute String user, @RequestAttribute String file, @PathVariable String name) {
+    public ResponseEntity<byte[]> getxls(@RequestParam String user, @RequestParam String file, @RequestParam String command, @PathVariable String name) {
         LOGGER.info("get report from user: {} and name: {}", user, file);
 
         HttpHeaders headers = new HttpHeaders();
@@ -98,9 +108,15 @@ public class PrintResource {
         headers.add("content-disposition", "inline;filename=" +
                 name);
         String path = Arrays.asList(printOutputDir, user, file).stream().collect(Collectors.joining(fileSeparator));
+        File fileXLS = new File(path);
         try {
-			return new ResponseEntity<>(IOUtils.toByteArray(new FileInputStream(new File(path))),
-			        headers, HttpStatus.OK);
+        	if (command != null && command.equalsIgnoreCase("delete")) {
+        		fileXLS.delete();
+        		return new ResponseEntity<>(HttpStatus.OK);
+        	} else {
+    			return new ResponseEntity<>(IOUtils.toByteArray(new FileInputStream(fileXLS)),
+    			        headers, HttpStatus.OK);        		
+        	}
 		} catch (IOException e) {
 			LOGGER.error("Cannot find file: {}", path);
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
