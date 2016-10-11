8c6c7e3f73e41d61f35dfa8c18175aadd400d38b
marco.spasiano <marco.spasiano@cnr.it>
2016-10-11 12:31:36 +0200

    Fix date

--- src/main/java/it/cnr/si/config/DatabaseConfiguration.java
+++ src/main/java/it/cnr/si/config/DatabaseConfiguration.java
@@ -29,5 +29,4 @@ public class DatabaseConfiguration {
     	conn.setAutoCommit(autocommit);
         return conn;
     }
-
 }
--- src/main/java/it/cnr/si/exception/JasperRuntimeException.java
+++ src/main/java/it/cnr/si/exception/JasperRuntimeException.java
@@ -4,7 +4,12 @@ package it.cnr.si.exception;
  * Created by francesco on 09/09/16.
  */
 public class JasperRuntimeException extends RuntimeException {
-    public JasperRuntimeException(String message, Throwable cause) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JasperRuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
 }
