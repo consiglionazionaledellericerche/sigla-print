d823492142f19c01d1cb71ef35869c8a8ef553e6
Francesco Uliana <francesco.uliana@cnr.it>
2016-10-13 10:29:30 +0200

    configurazione esternalizzata

--- src/main/java/it/cnr/si/config/HazelcastConfiguration.java
+++ src/main/java/it/cnr/si/config/HazelcastConfiguration.java
@@ -1,11 +1,5 @@
 package it.cnr.si.config;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.ManagementCenterConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -15,22 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HazelcastConfiguration {
 
-    @Bean
-    public Config config() {
-        Config config = new Config();
-        config.setInstanceName("sigla-print-server");
-        ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig();
-        managementCenterConfig.setEnabled(true);
-        managementCenterConfig.setUrl("http://localhost:8980");
-        config.setManagementCenterConfig(managementCenterConfig);
-        return config;
-    }
-
-    @Bean
-    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-        HazelcastCacheManager cacheManager = new HazelcastCacheManager(hazelcastInstance);
-        return cacheManager;
-    }
-
+    //TODO: obsoleto?
 
 }
