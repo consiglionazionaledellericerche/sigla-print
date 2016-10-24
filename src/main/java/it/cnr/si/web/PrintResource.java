a0a5c1b8c76a5e5c4c36701e25f3b8df7dd0c6bd
Francesco Uliana <francesco.uliana@cnr.it>
2016-10-24 15:46:55 +0200

    stacktraces

--- src/main/java/it/cnr/si/web/PrintResource.java
+++ src/main/java/it/cnr/si/web/PrintResource.java
@@ -5,6 +5,16 @@ import it.cnr.si.dto.Commit;
 import it.cnr.si.dto.HookRequest;
 import it.cnr.si.service.CacheService;
 import it.cnr.si.service.PrintService;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -16,23 +26,6 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by francesco on 09/09/16.
@@ -84,7 +77,7 @@ public class PrintResource {
 			return new ResponseEntity<>(IOUtils.toByteArray(new FileInputStream(new File(path))),
 			        headers, HttpStatus.OK);
 		} catch (IOException e) {
-			LOGGER.error("Cannot find file: {}", path);
+			LOGGER.error("Cannot find file: {}", path, e);
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 
@@ -118,7 +111,7 @@ public class PrintResource {
     			        headers, HttpStatus.OK);        		
         	}
 		} catch (IOException e) {
-			LOGGER.error("Cannot find file: {}", path);
+			LOGGER.error("Cannot find file: {}", path, e);
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
     }   
