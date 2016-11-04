93be127c214e93b020bfebc8f68249013ad0a505
marco.spasiano <marco.spasiano@cnr.it>
2016-11-04 19:00:46 +0100

    BUG sulle schedulazioni

--- src/main/java/it/cnr/si/repository/ExcelRepository.java
+++ src/main/java/it/cnr/si/repository/ExcelRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExcelRepository extends CrudRepository<ExcelSpooler, Long> {
 	@Query("SELECT MIN(pgEstrazione) FROM ExcelSpooler WHERE "
-			+ "((stato = 'C' and dtProssimaEsecuzione is null) or (stato IN ('C', 'S') and dtProssimaEsecuzione < SYSADTE))")	
+			+ "((stato = 'C' and dtProssimaEsecuzione is null) or (stato IN ('C', 'S') and dtProssimaEsecuzione < SYSDATE))")	
 	public Long findExcelToExecute();
 	
     @Query("select p.pgEstrazione from ExcelSpooler p, ParametriEnte e  where e.attivo = 'Y' and p.stato = 'S' and p.dtProssimaEsecuzione is null AND TRUNC(SYSDATE - p.duva) > Nvl(e.cancellaStampe,30)")
