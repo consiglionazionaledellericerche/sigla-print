62d7101f0b9c508f14df896d596169f874f8ecc1
Pagano <raffaele.pagano@cnr.it>
2017-03-06 13:04:54 +0100

    Nuova stampa Rendiconto Finanziario

---
+++ src/main/java/it/cnr/jasperreports/scriptlet/sigla/Stampa_pdg_rendicontoScriptlet.java
@@ -0,0 +1,38 @@
+package it.cnr.jasperreports.scriptlet.sigla;
+import net.sf.jasperreports.engine.JRDefaultScriptlet;
+import net.sf.jasperreports.engine.JRParameter;
+import net.sf.jasperreports.engine.JRScriptletException;
+
+
+public class Stampa_pdg_rendicontoScriptlet extends JRDefaultScriptlet {
+    
+	/** Creates a new instance of JRIreportDefaultScriptlet */
+	public Stampa_pdg_rendicontoScriptlet() {
+	}
+	public void beforeReportInit() throws JRScriptletException{
+		java.sql.Connection conn = (java.sql.Connection)getParameterValue(JRParameter.REPORT_CONNECTION);
+		java.sql.CallableStatement cs = null; 
+		try{	        
+			cs = conn.prepareCall("{call PRC_LOAD_TABLE_STAMPA_RENDIC(?,?,?,?,?,?,?,?)}");
+			cs.setObject(1,(java.lang.Integer)getParameterValue("P_ESERCIZIO") );
+			cs.setObject(2,(java.lang.String)getParameterValue("P_TIPO"));
+			cs.setObject(3,(java.lang.Integer)getParameterValue("P_NUM_LIV"));
+			cs.setObject(4,(java.lang.String)getParameterValue("P_TIPO_AGGREGAZIONE"));
+			cs.setObject(5,(java.lang.String)getParameterValue("P_TIPO_RENDICONTO"));
+			cs.setObject(6,(java.lang.String)getParameterValue("P_ORIGINE"));
+			cs.setObject(7,String.valueOf("N")); //P_COMPLETA
+			cs.setObject(8,String.valueOf("N")); //P_AGG_TABLE_TEMP
+			cs.setObject(9,String.valueOf("SYSTEM")); //P_UTCR
+			cs.executeQuery();
+		}catch (Throwable e) {
+			throw new JRScriptletException(e.getMessage());
+		} finally {
+			if (cs != null) 
+				try {
+					cs.close();
+				} catch (java.sql.SQLException e1) {
+					throw new JRScriptletException(e1.getMessage());
+				}
+		}		
+	}
+}
