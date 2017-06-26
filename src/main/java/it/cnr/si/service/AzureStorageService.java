64328e53ecad1bc1b092951d2ecd9c7b140eefaf
Francesco Uliana <francesco.uliana@cnr.it>
2017-06-26 10:29:48 +0200

    implementazione FilesystemStorageService

--- src/main/java/it/cnr/si/service/AzureStorageService.java
+++ src/main/java/it/cnr/si/service/AzureStorageService.java
@@ -1,34 +1,36 @@
 package it.cnr.si.service;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Created by francesco on 23/06/17.
  */
 
 @Service
+@ConditionalOnProperty("azure.foo.bar")
 public class AzureStorageService implements StorageService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AzureStorageService.class);
 
-
     @Override
-    public void write(String id, OutputStream outputStream) {
+    public void write(String id, byte[] byteArray) {
+        throw new NotImplementedException("cannot write " + id);
 
     }
 
     @Override
-    public void delete(String id) {
-
+    public boolean delete(String id) {
+        throw new NotImplementedException("cannot delete " + id);
     }
 
     @Override
     public InputStream get(String id) {
-        return null;
+        throw new NotImplementedException("cannot get " + id);
     }
 }
--- src/main/java/it/cnr/si/service/StorageService.java
+++ src/main/java/it/cnr/si/service/StorageService.java
@@ -1,16 +1,15 @@
 package it.cnr.si.service;
 
 import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Created by francesco on 23/06/17.
  */
 public interface StorageService {
 
-    void write(String id, OutputStream outputStream);
+    void write(String id, byte[] byteArray);
 
-    void delete(String id);
+    boolean delete(String id);
 
     InputStream get(String id);
 }
