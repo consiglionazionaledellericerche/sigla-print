86082d04106d94bf70719135e59caa9a84d01663
marco.spasiano <marco.spasiano@cnr.it>
2020-01-15 09:59:12 +0100

    FIX Web Security csrf disable

--- src/main/java/it/cnr/si/config/WebSecurityConfig.java
+++ src/main/java/it/cnr/si/config/WebSecurityConfig.java
@@ -27,6 +27,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
     }
 }
